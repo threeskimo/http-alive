7fc619e8d669ad6b45ba40850c54aa1eedacd8ab
threeskimo <77468289+threeskimo@users.noreply.github.com>
2021-07-20 22:13:25 -0500

    Added online/offline counter

--- http-alive.sh
+++ http-alive.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 filename=$1
-line=1
+line=0
+online=0
+offline=0
 
 # Check if file exists
 if [[ -f "$filename" ]]; then
@@ -21,21 +23,25 @@ else
 	echo "\"results.txt\" will be created."
 fi
 
-echo -e "\033[1;33m-----------------------------------------------------------\033[0m"
+echo -e "\033[1;33m-------------------------------------------------------------------------------\033[0m"
 
 # CURL us some URLs
 while read TARGET; do
 	if curl --silent --output /dev/null -I --connect-timeout 3 --max-time 10 "$TARGET"; then
-		printf "[$line/$total] $TARGET\t: \033[0;32monline\033[0m\n" | expand -t 50
+		line=$((line+1))
+		online=$((online+1))
+		printf "[$line/$total] $TARGET\t: \033[0;32monline  ($online)\033[0m\n" | expand -t 65
 		echo -e "$TARGET:online" >> results.txt
-		line=$((line+1))
+		
 	else
-		printf "[$line/$total] $TARGET\t: \033[0;31moffline\033[0m\n" | expand -t 50
-		echo -e "$TARGET:offline" >> results.txt
 		line=$((line+1))
+		offline=$((offline+1))
+		printf "[$line/$total] $TARGET\t: \033[0;31moffline ($offline)\033[0m\n" | expand -t 65
+		echo -e "$TARGET:offline" >> results.txt
 	fi
 done <$filename
 
 #Spit out results
-echo -e "\033[1;33m-----------------------------------------------------------"
+echo -e "\033[1;33m-------------------------------------------------------------------------------"
+echo -e "\033[0monline: \033[0;32m$online \033[0m| offline: \033[0;31m$offline\033[0m"
 echo -e "\033[0mResults stored in file \"results.txt\""
